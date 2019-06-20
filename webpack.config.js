639afc7f810bff3da891938043c2f1d067318288
Roman Jędras <romanjedras@o2.pl>
2019-06-20 15:27:40 +0200

    Add loadr babelto webpack conf.

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,17 @@ const path = require('path');
 
 
 module.exports = {
-	entry: './main.js',
+	entry: './src/app.js',
 	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname,'')
+		path: path.resolve(__dirname,'build'),
+		filename: "app.bundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: "babel-loader"
+			}
+		]
 	}
 }
